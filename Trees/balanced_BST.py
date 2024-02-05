40d12f60cf1736b1a1bafbe5ccce6ece36654c22
Dmitriy Trifonov <dmitriy.trifonov@vk.team>
2024-02-05 10:42:10 +0300

    add bBST class

---
+++ Trees/balanced_BST.py
@@ -0,0 +1,44 @@
+class BSTNode:
+
+    def __init__(self, key, parent):
+        self.NodeKey = key
+        self.Parent = parent
+        self.LeftChild = None
+        self.RightChild = None
+        self.Level = 0
+
+
+class BalancedBST:
+
+    def __init__(self):
+        self.Root = None
+
+    def GenerateTree(self, a):
+        sorted_array = sorted(a)
+        self.Root = _build_tree(sorted_array, None, 0)
+
+    def IsBalanced(self, root_node):
+        return _get_balanced_depth(root_node) is not None
+
+
+def _build_tree(sorted_array, parent, level):
+    if len(sorted_array) == 0:
+        return None
+    mid = len(sorted_array) // 2
+    node = BSTNode(sorted_array[mid], parent)
+    node.Level = level
+    node.LeftChild = _build_tree(sorted_array[:mid], node, level + 1)
+    node.RightChild = _build_tree(sorted_array[mid + 1:], node, level + 1)
+    return node
+
+
+def _get_balanced_depth(node):
+    if node is None:
+        return 0
+    left_depth = _get_balanced_depth(node.LeftChild)
+    right_depth = _get_balanced_depth(node.RightChild)
+    if left_depth is None or right_depth is None:
+        return None
+    if abs(left_depth - right_depth) > 1:
+        return None
+    return max(left_depth, right_depth) + 1
